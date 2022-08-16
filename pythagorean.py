9674da8cc6c42d955362e94f82448123d77c876d
Daniel Afanador <danielthemisfits@gmail.com>
2022-08-15 21:26:51 -0300

    Programa para devolver múltiples variables de una misma función

---
+++ pythagorean.py
@@ -0,0 +1,8 @@
+def pythagorean(a, b):
+    a_squared = a * a
+    b_squared = b * b
+    c_squared = a_squared + b_squared
+    
+    return a_squared, b_squared, c_squared
+
+print (pythagorean(5, 12))
